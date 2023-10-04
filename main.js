ece1f8a3fe7a7747044d58ddb1c10a5fa2a985a4
chrisstrom1 <wchrisstrom@yahoo.com>
2023-10-04 13:34:27 -0500

    first commit

--- main.js
+++ main.js
@@ -9,9 +9,63 @@ const jobTypes = {
   programmer: 'Any Ship!'
 };
 
-// Your code will go here
+// Begin by reading the tests in the bottom of the file. Make sure you understand them.
+// Build a class for CrewMember.
+// can enter ship
+// should have a name, a job, a specialSkill and ship upon instantiation', 
+class CrewMember {
 
-update 
+  constructor (name, job, specialSkill, ship) {
+
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship=ship;
+  }
+
+  enterShip =(boat) => {
+
+  this.ship = boat;
+  //take the incoming ship push it into the ships crew arary fo this crew member
+  this.ship.crew.push(this)
+
+  }
+
+
+}
+
+
+// Build a class for Ship.
+// should have a name, a type, an ability and an empty crew upon instantiation'
+class Ship{
+
+  constructor (name, type, ability, crew) {
+  this.name = name;
+  this.type = type;
+  this.ability = ability;
+  this.crew = [];
+
+}
+missionStatement = () => {
+
+//need at least 1 crew member to perform a mission so the crew aray length 
+if (this.crew.length ===0) {
+  return "Can't perform a mission yet."
+
+}
+else {
+
+  return this.ability
+}
+}
+}
+
+
+// Make sure CrewMembers can enter Ships.
+// Make sure to return a mission statement.
+// Turn in the repo URL
+
+
 
 
 
@@ -53,6 +107,60 @@ if (typeof describe === 'function'){
     });
 
     it('can return a mission statement correctly', function(){
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+      const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+      let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
+      const crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology');
+      assert.equal(mav.missionStatement(), "Can't perform a mission yet.");
+      assert.equal(hermes.missionStatement(), "Can't perform a mission yet.");
+
+      crewMember1.enterShip(mav);
+      assert.equal(mav.missionStatement(), "Ascend into low orbit");
+
+      crewMember2.enterShip(hermes);
+      assert.equal(hermes.missionStatement(), "Interplanetary Space Travel");
+    });
+  });
+}
+
+
+
+// Begin by reading the tests and building a function that will full each one.
+// As you build, you might not have to build them in order, maybe you do...
+// These are the tests
+if (typeof describe === 'function') {
+  describe('CrewMember', function () {
+    it('should have a name, a job, a specialSkill and ship upon instantiation', function () {
+      // this creates a CrewMember and passes the following arguments into its constructor:
+      // 'Rick Martinez', 'pilot', 'chemistry'
+      const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+      assert.equal(crewMember1.name, 'Rick Martinez');
+      assert.equal(crewMember1.job, 'pilot');
+      assert.equal(crewMember1.specialSkill, 'chemistry');
+      assert.equal(crewMember1.ship, null);
+    });
+
+    it('can enter a ship', function () {
+      // this creates a new Ship. Can you build a class that can be called so that this Ship can be built?
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+      const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+      crewMember1.enterShip(mav);
+      assert.equal(crewMember1.ship, mav);
+      assert.equal(mav.crew.length, 1);
+      assert.equal(mav.crew[0], crewMember1);
+    });
+  });
+
+  describe('Ship', function () {
+    it('should have a name, a type, an ability and an empty crew upon instantiation', function () {
+      let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+      assert.equal(mav.name, 'Mars Ascent Vehicle');
+      assert.equal(mav.type, 'MAV');
+      assert.equal(mav.ability, 'Ascend into low orbit');
+      assert.equal(mav.crew.length, 0);
+    });
+
+    it('can return a mission statement correctly', function () {
       let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
       const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
       let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
